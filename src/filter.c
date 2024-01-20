188c3fec6d9bee176766311390a568a0068885d4
Maxime Sirbu <m.sirbu@expressivee.com>
2024-01-20 02:56:58 +0100

    improve filter algorithm

--- src/filter.c
+++ src/filter.c
@@ -13,9 +13,12 @@
 #ifdef FILTER_VECSIZE
 
 typedef struct {
-    float b[3][FILTER_VECSIZE];
-    float a[3][FILTER_VECSIZE];
-    float s[2][FILTER_VECSIZE];
+    float b[3][FILTER_VECSIZE]
+        __attribute__((aligned(sizeof(float) * FILTER_VECSIZE)));
+    float a[3][FILTER_VECSIZE]
+        __attribute__((aligned(sizeof(float) * FILTER_VECSIZE)));
+    float s[2][FILTER_VECSIZE]
+        __attribute__((aligned(sizeof(float) * FILTER_VECSIZE)));
 } filter(_t);
 
 void filter(_butterworth_lp)(filter(_t) * filter, float samplerate, float fr);
@@ -53,24 +56,28 @@ void filter(_sos_analog)(filter(_t) filter[], const float ba[][2][3],
      * we use second order section filters (sos) for stability
      */
 
+    float c[FILTER_VECSIZE];
+    float csq[FILTER_VECSIZE];
+    /* bilinear transform */
     for (int i = 0; i < FILTER_VECSIZE; ++i) {
-        /* bilinear transform */
-        float w1  = 2.f * M_PI * freqs[i];
-        float c   = 1 / tanf(w1 * 0.5f / samplerate);
-        float csq = c * c;
-        for (int j = 0; j < nsos; ++j) {
+        float w1 = 2.f * M_PI * freqs[i];
+        c[i]     = 1 / tanf(w1 * 0.5f / samplerate);
+        csq[i]   = c[i] * c[i];
+    }
+    for (int j = 0; j < nsos; ++j) {
+        for (int i = 0; i < FILTER_VECSIZE; ++i) {
             assert(ba[j][1][0] == 1.f);
             float a0  = ba[j][1][2];
             float a1  = ba[j][1][1];
             float b0  = ba[j][0][2];
             float b1  = ba[j][0][1];
             float b2  = ba[j][0][0];
-            float d   = 1.f / (a0 + a1 * c + csq);
-            float b0d = (b0 + b1 * c + b2 * csq) * d;
-            float b1d = 2 * (b0 - b2 * csq) * d;
-            float b2d = (b0 - b1 * c + b2 * csq) * d;
-            float a1d = 2 * (a0 - csq) * d;
-            float a2d = (a0 - a1 * c + csq) * d;
+            float d   = 1.f / (a0 + a1 * c[i] + csq[i]);
+            float b0d = (b0 + b1 * c[i] + b2 * csq[i]) * d;
+            float b1d = 2 * (b0 - b2 * csq[i]) * d;
+            float b2d = (b0 - b1 * c[i] + b2 * csq[i]) * d;
+            float a1d = 2 * (a0 - csq[i]) * d;
+            float a2d = (a0 - a1 * c[i] + csq[i]) * d;
 
             filter[j].b[0][i] = b0d;
             filter[j].b[1][i] = b1d;
@@ -78,6 +85,21 @@ void filter(_sos_analog)(filter(_t) filter[], const float ba[][2][3],
             filter[j].a[0][i] = 1.f;
             filter[j].a[1][i] = a1d;
             filter[j].a[2][i] = a2d;
+        }
+    }
+
+    /* normalize so that all discret b0 except the first one
+     * is equal to 1.0 */
+    for (int j = 1; j < nsos; ++j) {
+        for (int i = 0; i < FILTER_VECSIZE; ++i) {
+            float b0 = filter[j].b[0][i];
+            filter[0].b[0][i] *= b0;
+            filter[0].b[1][i] *= b0;
+            filter[0].b[2][i] *= b0;
+            float b0div       = 1.f / b0;
+            filter[j].b[0][i] = 1.f;
+            filter[j].b[1][i] *= b0div;
+            filter[j].b[2][i] *= b0div;
         }
     }
 }
@@ -89,7 +111,8 @@ static inline void filter(_process)(filter(_t) * filter,
     y = (float *)__builtin_assume_aligned(y, sizeof(float) * FILTER_VECSIZE);
 
     for (int j = 0; j < nsos; ++j) {
-        /* use direct form II */
+/* use direct form II */
+#pragma omp simd
         for (int i = 0; i < FILTER_VECSIZE; ++i) {
             float a1 = filter[j].a[1][i];
             float a2 = filter[j].a[2][i];
@@ -100,7 +123,10 @@ static inline void filter(_process)(filter(_t) * filter,
             float s1 = filter[j].s[0][i];
             float s2 = filter[j].s[1][i];
             float s0 = y[i] - a1 * s1 - a2 * s2;
-            y[i]     = b0 * s0 + b1 * s1 + b2 * s2;
+
+            if (j == 0) y[i] = b0 * s0 + b1 * s1 + b2 * s2;
+            else
+                y[i] = s0 + b1 * s1 + b2 * s2;
 
             filter[j].s[1][i] = filter[j].s[0][i];
             filter[j].s[0][i] = s0;

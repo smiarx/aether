ae5b13db1e0ecc9bde2271eab0c2f115b5b07c1f
MS <maxime.sirbu@disroot.org>
2024-04-17 13:53:02 +0200

    remove labels for springs detail parameters

--- src/GUI/SpringsSection.cpp
+++ src/GUI/SpringsSection.cpp
@@ -147,9 +147,8 @@ SpringsSection::Spring::Spring(juce::AudioProcessorValueTreeState &apvts,
 
     params[0].getSlider().setSliderStyle(
         juce::Slider::SliderStyle::LinearHorizontal);
-    /* remove label for volume and pan */
-    params[0].setLabelVisiblie(false);
-    params[1].setLabelVisiblie(false);
+    /* remove label for parameters */
+    for (auto &param : params) param.setLabelVisiblie(false);
 
     for (auto &p : params) {
         addAndMakeVisible(p);
